6971eb16daf9904664ccce82d7149608904f0bd4
radek <yakuzatanaka@gmail.com>
2020-06-18 16:03:15 +0200

    added put and delete

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -22,4 +23,23 @@ public class FakePersonDataAccessService implements PersonDao{
     public List<Person> selectAllPeople() {
         return DB;
     }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+
 }
